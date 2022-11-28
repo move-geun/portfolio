a795ef8ede80fe7ab3eebabfe0f368f189a11271
이동근 <ehdrms1426@gmail.com>
2022-11-28 12:02:50 +0900

    add : basic react app

---
+++ src/styles/global.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'MICEGothic Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2206-01@1.0/MICEGothic Bold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+}
+body{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'MICEGothic Bold';
+    height: 100%;
+      ::-webkit-scrollbar {
+    display: none;
+  }
+}
+a{
+    text-decoration: none;
+    color: black;
+}
+ul{
+    padding: 0;
+    margin: 0;
+}
+li{
+    list-style: none;
+}
+input, button{
+    border:none;
+    background-color: white;
+    white-space: nowrap;
+}
+input:focus,
+input:active,
+button:focus,
+button:active {
+  outline: none;
+  box-shadow: none;
+}
+div {
+    white-space: nowrap;
+}
+`;
+
+export default GlobalStyle;
